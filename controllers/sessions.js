7748d94886df9ca460eb9baa6a57d7515adeb34b
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-04-17 19:52:25 -0400

    working auth

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,24 +4,30 @@ var User = require('../models/users.js');
 var bcrypt = require('bcrypt');
 
 router.get('/new', function(req, res){
-    res.render('sessions/new.ejs');
+  res.render('sessions/new.ejs');
 });
 
 router.post('/', function(req, res){
-    User.findOne({ username: req.body.username }, function(err, foundUser){
-        if( bcrypt.compareSync(req.body.password, foundUser.password) ){
-            req.session.currentuser = foundUser;
-            res.redirect('/');
-        } else {
-            res.send('wrong password');
-        }
-    });
+  User.findOne({ username: req.body.username }, function(err, foundUser){
+    if (!foundUser){
+      res.render('sessions/tryagain.ejs');
+    }
+    else if (foundUser){
+      if (bcrypt.compareSync(req.body.password, foundUser.password)){
+        req.session.currentuser = foundUser;
+        res.redirect('/');
+      } else {
+        // res.send('wrong username or password');
+        res.render('sessions/tryagain.ejs');
+      };
+    };
+  });
 });
 
 router.delete('/', function(req, res){
-    req.session.destroy(function(){
-        res.redirect('/');
-    });
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
 })
 
 module.exports = router;

d6d2331afc8d34891e021555097d2106de7a78d9
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-04-19 12:44:30 -0400

    routes are protected

--- controllers/wificity.js
+++ controllers/wificity.js
@@ -9,52 +9,72 @@ var Transceivers = require('../models/transceivers.js');
 ///////////////////////////Application Routes////////////////////////////////////
 //get route for displaying index of applications
 router.get('/applications', function(req, res){
-  Applications.find({}, function(error, allApplications){
+  if(req.session.currentuser){
+    Applications.find({}, function(error, allApplications){
       res.render('applications/index.ejs', {
-          applications: allApplications,
-          currentUser: req.session.currentuser
-
+        applications: allApplications,
+        currentUser: req.session.currentuser
       });
-  });
+    });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 //get route for creating new application
 router.get('/applications/newapplication', function(req, res){
+  if(req.session.currentuser){
     res.render('applications/new.ejs', {
         currentUser: req.session.currentuser
     });
+  } else {
+      res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 //post route for creating new application
 router.post('/applications', function(req, res){
+  if(req.session.currentuser){
     Applications.create(req.body, function(){
         res.redirect('applications/index');
     });
+  } else {
+      res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 //get route for displaying index of applications
 router.get('/applications/index', function(req, res){
-  Applications.find({}, function(error, allApplications){
+  if(req.session.currentuser){
+    Applications.find({}, function(error, allApplications){
       res.render('applications/index.ejs', {
           applications: allApplications,
           currentUser: req.session.currentuser
       });
   });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 //delete route for deleting application
 router.delete('/applications/deleteapplication/:id', function(req, res){
+  if(req.session.currentuser){
     Applications.findByIdAndRemove(req.params.id, function(err, data){
         res.redirect('/wificity/applications/index')
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 //get route for editing application
 router.get('/applications/:id/edit', function(req, res){
+  if(req.session.currentuser){
     Applications.findById(req.params.id, function(err, foundApplication){ //find the application
         res.render(
     		'applications/edit.ejs',
@@ -63,65 +83,93 @@ router.get('/applications/:id/edit', function(req, res){
           currentUser: req.session.currentuser
     		});
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 
 //put route for editing application
 router.put('/applications/:id', function(req, res){
+  if(req.session.currentuser){
     //{new: true} tells mongoose to send the updated model into the callback
     Applications.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, updatedModel){
         res.redirect('/wificity/applications/index');
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 //get route for showing application detail
 router.get('/applications/:id', function(req, res){
+  if(req.session.currentuser){
     Applications.findById(req.params.id, function(err, foundApplication){
         res.render('applications/show.ejs', {
             application:foundApplication,
             currentUser: req.session.currentuser
         });
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 //////////////////////////Transceiver Routes/////////////////////////////////////
 //get route for creating new transceiver
 router.get('/transceivers/newtransceiver', function(req, res){
+  if(req.session.currentuser){
     res.render('transceivers/new.ejs', {
       currentUser: req.session.currentuser
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 //post route for creating new transceiver
 router.post('/transceivers', function(req, res){
+  if(req.session.currentuser){
     Transceivers.create(req.body, function(){
         res.redirect('transceivers/index');
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 //get route for displaying index of transceivers
 router.get('/transceivers/index', function(req, res){
-  Transceivers.find({}, function(error, allTransceivers){
-      res.render('transceivers/index.ejs', {
-          transceivers: allTransceivers,
-          currentUser: req.session.currentuser
-      });
-  });
+  if(req.session.currentuser){
+    Transceivers.find({}, function(error, allTransceivers){
+        res.render('transceivers/index.ejs', {
+            transceivers: allTransceivers,
+            currentUser: req.session.currentuser
+        });
+    });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 //delete route for deleting transceiver
 router.delete('/transceivers/deletetransceiver/:id', function(req, res){
+  if(req.session.currentuser){
     Transceivers.findByIdAndRemove(req.params.id, function(err, data){
-        res.redirect('/wificity/transceivers/index')
+        res.redirect('/wificity/transceivers/index');
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 //get route for editing transceiver
 router.get('/transceivers/:id/edit', function(req, res){
+  if(req.session.currentuser){
     Transceivers.findById(req.params.id, function(err, foundTransceiver){ //find the transceiver
         res.render(
     		'transceivers/edit.ejs',
@@ -130,26 +178,37 @@ router.get('/transceivers/:id/edit', function(req, res){
           currentUser: req.session.currentuser
     		});
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
 
 //put route for editing transceiver
 router.put('/transceivers/:id', function(req, res){
+  if(req.session.currentuser){
     //{new: true} tells mongoose to send the updated model into the callback
     Transceivers.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, updatedModel){
         res.redirect('/wificity/transceivers/index');
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 //get route for showing transceiver detail
 router.get('/transceivers/:id', function(req, res){
+  if(req.session.currentuser){
     Transceivers.findById(req.params.id, function(err, foundTransceiver){
         res.render('transceivers/show.ejs', {
             transceiver:foundTransceiver,
             currentUser: req.session.currentuser
         });
     });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 
@@ -163,8 +222,13 @@ router.post('/', function(req, res){
 
 //get route for displaying map
 router.get('/map', function(req, res){
-    res.render('map/footprintmap.ejs');
-    currentUser: req.session.currentuser
+  if(req.session.currentuser){
+    res.render('map/footprintmap.ejs', {
+      currentUser: req.session.currentuser
+    });
+  } else {
+    res.render('sessions/tryagain.ejs');
+  }
 });
 
 module.exports = router;

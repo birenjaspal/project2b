bdfbe5d0a6d5b01aa352b3c1d8cc4c11bd343536
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-04-20 03:51:46 -0400

    seeding data

--- models/applications.js
+++ models/applications.js
@@ -11,14 +11,14 @@ var applicationSchema = new Schema({
 var Application = mongoose.model('Application', applicationSchema);
 
 //uncomment the following code to populate, then recomment
-// var applicationData = require('../populateApplications.js');
-// console.log(applicationData);
-//
-// Application.collection.insertMany( applicationData,
-//   function( err, data ) {
-//     console.log( "added provided application data" );
-//     mongoose.connection.close();
-//   }
-// );
+var applicationData = require('../populateApplications.js');
+console.log(applicationData);
+
+Application.collection.insertMany( applicationData,
+  function( err, data ) {
+    console.log( "added provided application data" );
+    mongoose.connection.close();
+  }
+);
 
 module.exports = Application;
--- models/transceivers.js
+++ models/transceivers.js
@@ -13,13 +13,13 @@ var Transceiver = mongoose.model('Transceiver', transceiverSchema);
 
 
 //uncomment the following code to populate, then recomment
-// var transceiverData = require('../populateTransceivers.js');
-// console.log(transceiverData);
-// Transceiver.collection.insertMany( transceiverData,
-//   function( err, data ) {
-//     console.log( "added provided transceiver data" );
-//     mongoose.connection.close();
-//   }
-// );
+var transceiverData = require('../populateTransceivers.js');
+console.log(transceiverData);
+Transceiver.collection.insertMany( transceiverData,
+  function( err, data ) {
+    console.log( "added provided transceiver data" );
+    mongoose.connection.close();
+  }
+);
 
 module.exports = Transceiver;
